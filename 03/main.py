71b010ab867d84f317e064fadba1851c8e61dd76
vasilisagul <gulinavasilisa914@gmail.com>
2025-03-29 17:07:44 +0300

    Add files via upload

---
+++ 03/main.py
@@ -0,0 +1,60 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow
+import requests
+
+
+class MainWindow(QMainWindow):
+    g_map: QLabel
+    press_delta = 0.1
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi('main_window.ui', self)
+
+        self.map_zoom = 5
+        self.map_ll = [37.977751, 55.757718]
+        self.map_l = 'map'
+        self.map_key = ''
+
+        self.refresh_map()
+
+    def refresh_map(self):
+        map_params = {
+            "ll": ','.join(map(str, self.map_ll)),
+            "l": self.map_l,
+            'z': self.map_zoom
+        }
+        response = requests.get('https://static-maps.yandex.ru/1.x/', params=map_params)
+        with open('tmp.png', mode='wb') as tmp:
+            tmp.write(response.content)
+
+        pixmap = QPixmap()
+        pixmap.load('tmp.png')
+        self.g_map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key.Key_PageUp and self.map_zoom < 17:
+            self.map_zoom += 1
+        if key == Qt.Key.Key_PageDown and self.map_zoom > 0:
+            self.map_zoom -= 1
+        if key == Qt.Key.Key_Left:
+            self.map_ll[0] -= self.press_delta
+        if key == Qt.Key.Key_Right:
+            self.map_ll[0] += self.press_delta
+        if key == Qt.Key.Key_Up:
+            self.map_ll[1] += self.press_delta
+        if key == Qt.Key.Key_Down:
+            self.map_ll[1] -= self.press_delta
+
+        self.refresh_map()
+
+
+app = QApplication(sys.argv)
+main_window = MainWindow()
+main_window.show()
+sys.exit(app.exec())
